6856e9a2f5bb55b4812aa8b9f72d7142026d073e
alexamoura <alexmarciapynna@gmail.com>
2025-11-04 11:30:54 -0300

    Update bot_with_cookies.py

--- bot_with_cookies.py
+++ bot_with_cookies.py
@@ -2,7 +2,7 @@
 """
 bot_with_cookies_melhorado.py - Versão Profissional
 
-Telegram bot IA (webhook) com sistema de controle de downloads e suporte a pagamento PIX - ATUALIZADO EM 04/11/2025 - 10:00HS
+Telegram bot IA (webhook) com sistema de controle de downloads e suporte a pagamento PIX - ATUALIZADO EM 04/11/2025 - 11:30HS
 """
 import os
 import sys
