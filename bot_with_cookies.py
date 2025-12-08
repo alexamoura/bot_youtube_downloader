d45badb9d696b5289b8f05f60e0064865978cd34
alexamoura <alexmarciapynna@gmail.com>
2025-12-08 09:51:09 -0300

    Update bot_with_cookies.py

--- bot_with_cookies.py
+++ bot_with_cookies.py
@@ -4000,11 +4000,11 @@ async def _do_download(token: str, url: str, tmpdir: str, chat_id: int, pm: dict
                         except:
                             continue
             
-            # Envia o vídeo
-            MAX_RETRIES = 3
-            retry_delay = [1, 3, 5]
+# Envia o vídeo
+caption = "🎬 Aproveite o seu vídeo 🎬"
 
-            caption = "🎬 Aproveite o seu vídeo 🎬"
+MAX_RETRIES = 3
+retry_delay = [1, 3, 5]
 
 for attempt in range(MAX_RETRIES):
     fh = open(path, "rb")
